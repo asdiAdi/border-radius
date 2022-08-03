e3327656de98fb9602402732fa917049e2595188
Carl Timothy Adi <carltimothyadi@gmail.com>
2022-08-03 22:16:27 +0800

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import Slider from '@mui/material/Slider';
+import { alpha, styled } from '@mui/material/styles';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [value, setValue] = React.useState<number>(50);
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+    console.log(newValue)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center content-center bg-slate-300 flex-wrap">
+      <div className='w-full flex justify-center'>
+        <div className='bg-orange-400 h-24 w-24 rounded-[30%40%30%40%/20%20%30%30%] flex'>
+          <div className='w-full self-start -mt-4'>
+            <Slider value={value} onChange={handleChange} size="small" step={10} sx={{
+              padding: '0',
+              '& .MuiSlider-thumb': {
+                '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                  boxShadow: `0px 0px 0px 0px`,
+                },
+                '&:after': {
+                  borderRadius: '0px',
+                  width: '0px',
+                  height: '0px'
+                },
+                borderRadius: '0px',
+                height: '5px',
+                width: '5px',
+              },
+            }} />
+          </div>
+        </div>
+      </div>
+
+      <div className='w-24 '>
+
+      </div>
+
     </div>
   );
 }
-
-export default App;
