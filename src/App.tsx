475ba72a0e9bc032f2227e661161006717fc61d1
Carl Timothy Adi <carltimothyadi@gmail.com>
2022-08-05 23:22:28 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,55 +4,58 @@ import './App.css';
 import { TextField } from '@mui/material';
 import Box from '@mui/material/Box';
 
+type sliderType = {
+  'top-slider': any,
+  'bottom-slider': any,
+  'left-slider': any,
+  'right-slider': any
+}
+
 export default function App() {
-  const [topValue, setTopValue] = React.useState<number[]>([10, 90]);
-  const [topThumbSwitch, setTopThumbSwitch] = React.useState<boolean>(false);
-  const [currentActiveThumb, setCurrentActiveThumb] = React.useState<number>(0);
-  const [pastTarget, setPastTarget] = React.useState<string>("bottom-slider");
-  const [bottomValue, setBottomValue] = React.useState<number[]>([10, 90]);
-  const [borderRadius, setBorderRadius] = React.useState<string[]>(['10%', '10%', '0%', '0%', '/', '40%', '40%', '0%', '0%']);
-  const handleChange = (event: Event | React.SyntheticEvent<Element, Event>, newValue: number | number[], activeThumb?: number) => {
-    let [topLeft, topRight, bottomRight, bottomLeft, slash, leftTop, rightTop, rightBottom, leftBottom] = borderRadius;
-    let targetName = (event.target as HTMLInputElement).name;
-    let eventValue = (event.target as HTMLInputElement).value as unknown as number[];
-    if (event.type === "mousedown" && activeThumb !== undefined) {
-      setCurrentActiveThumb(activeThumb);
-    } else if (activeThumb != undefined && activeThumb !== currentActiveThumb) {
-      setCurrentActiveThumb(currentActiveThumb === 0 ? 1 : 0);
-      if (targetName === "top-slider" || pastTarget === "top-slider") setTopThumbSwitch(!topThumbSwitch);
-    }
-    if (targetName === "top-slider" || pastTarget === "top-slider") {
-      // console.log(eventValue);
-      // console.log(topValue);
-      // console.log("")
-      setTopValue(newValue as number[]);
-      if (!topThumbSwitch) {
-        topLeft = topValue[0].toString() + '%';
-        topRight = (100 - topValue[1]).toString() + '%';
-      } else {
-        topLeft = topValue[1].toString() + '%';
-        topRight = (100 - topValue[0]).toString() + '%';
-      }
-      // if (topLeft < '0') topLeft = '0%';
-      // if (topRight < '0') topRight = '0%';
-    } else if (targetName === "bottom-slider") {
-      setBottomValue(newValue as number[]);
-    }
-    let x = newValue as number[];
-    if (x[0] < 0) {setTopValue([0, x[1]]);}
-    if (x[1] < 0) setTopValue([x[0], 0]);
-    console.log(topValue)
-    console.log(newValue)
-    console.log("")
-    setBorderRadius([topLeft, topRight, bottomRight, bottomLeft, slash, leftTop, rightTop, rightBottom, leftBottom]);
-    setPastTarget(targetName);
+  const [value, setValue] = React.useState<sliderType>({
+    'top-slider': [10, 90],
+    'bottom-slider': [10,10],
+    'left-slider': [10,10],
+    'right-slider': [10,10]
+  });
+  // const [thumbSwitch, setThumbSwitch] = React.useState<sliderType>({
+  //   'top-slider': false,
+  //   'bottom-slider': false,
+  //   'left-slider': false,
+  //   'right-slider': false
+  // });
+  // const [currentActiveThumb, setCurrentActiveThumb] = React.useState<number>(0);
+  // const [topValue, setTopValue]= React.useState(value['top-slider']);
+  React.useEffect(()=> {
+    // setBorderRadius([topLeft, topRight, bottomRight, bottomLeft, slash, leftTop, rightTop, rightBottom, leftBottom]);
+    // let borderRadius = `${value['top-slider'][0]}% ${100-value['top-slider'][1]}% ${100-value['bottom-slider'][1]}% ${value['bottom-slider'][0]}% / ${value['left-slider'][0]}% ${100-value['right-slider'][1]}% ${value['right-slider'][0]}% ${100-value['left-slider'][1]}%`;
+    // document.getElementById('box')!.style.borderRadius = `${value['top-slider'][0]}% ${100-value['top-slider'][1]}% 10% 10% / 10% 10% 10% 10%`;
+  });
+  const handleChange = (event: Event | React.SyntheticEvent<Element, Event>, newValue: number | number[], target:keyof(sliderType), activeThumb?: number) => {
+    newValue = newValue as number[]; 
+    // if (event.type === "mousedown" && activeThumb !== undefined) {
+    //   setCurrentActiveThumb(activeThumb);
+    // } 
+    // else if (activeThumb !== currentActiveThumb && activeThumb !== undefined) {
+    //   thumbSwitch[target]  = !thumbSwitch[target];
+    //   setThumbSwitch(thumbSwitch);
+    //   setCurrentActiveThumb(activeThumb);
+    // }
+    // if(thumbSwitch[target]) [newValue[0], newValue[1]] = [newValue[1], newValue[0]];
+    // if (newValue[0] < 0) value[target] = [0, newValue[1]];
+    // else if (newValue[1] > 100) value[target] = [newValue[0], 100];
+    // else value[target] = newValue;
+    value[target] = newValue;
+    
+    setValue(value);
+    // setTopValue(value['top-slider'])
   };
 
   return (
     <div className="h-screen flex justify-center content-center bg-slate-300 flex-wrap">
       <div className='w-full flex justify-center flex-wrap'>
         <div className='w-[60rem] h-min flex justify-center'>
-          <Slider id="top-slider" name="top-slider" value={topValue} onChange={handleChange} onChangeCommitted={handleChange} size="medium" step={1} min={-100} max={200} marks={[{value: 0},{value: 100}]} valueLabelDisplay='on' sx={{
+          <Slider value={value['top-slider']} onChange={(e, v, a) => handleChange(e, v, 'top-slider', a)}  onChangeCommitted={(e, v) => handleChange(e, v, 'top-slider')} size="medium" step={1} min={-100} max={200} marks={[{value: 0},{value: 100}]}  sx={{
             padding: '0',
             '& .MuiSlider-thumb': {
               '&:hover, &.Mui-focusVisible, &.Mui-active': {
@@ -77,7 +80,7 @@ export default function App() {
           }} />
         </div>
         <div className='w-full h-min'></div>
-        <Box className='bg-orange-400 w-80 aspect-square flex flex-wrap' sx={{ borderRadius: borderRadius.join(' ') }}/>
+        <Box id='box' className='bg-orange-400 w-80 aspect-square flex flex-wrap'/>
         <div className='w-full flex justify-center mt-4'>
           <TextField label="border-radius" size="small" defaultValue={"hello world"} InputProps={{ readOnly: true }} />
         </div>
